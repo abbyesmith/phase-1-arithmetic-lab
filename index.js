f8cd4ef0b61627477df82aaa34c045386a08d5ef
Abby <abby.e.smith.85@gmail.com>
2023-01-30 22:33:23 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 1
+let num2 = 62
 
+let multiply= num1 * num2
+ 
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 14
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max(3, 4, 20)
 // Write your code here
